d3867f0daa8e4c3702220e00f8fa982e1c04e3f0
William Crupi <crupib@gmail.com>
2024-03-12 15:35:29 -0400

    ansible

--- ansible-playbook-essentials/learn/ansible/.vagrant/bundler/global.sol
+++ ansible-playbook-essentials/learn/ansible/.vagrant/bundler/global.sol
@@ -1,1 +1,1 @@
-{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.31.0"]]],"checksum":"37f5f79c0c7d25d25a62b060a9cf83d6c66593a5e52bef641965f7767af527ef","vagrant_version":"2.4.0"}
+{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.31.0"]]],"checksum":"37f5f79c0c7d25d25a62b060a9cf83d6c66593a5e52bef641965f7767af527ef","vagrant_version":"2.4.1"}
